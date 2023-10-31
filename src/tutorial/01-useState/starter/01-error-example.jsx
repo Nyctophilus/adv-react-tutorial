7e296ab84a68ee797d2a9e0abe5d773ef9e169a8
Medo <mohammed.yuossry@gmail.com>
2023-10-31 15:39:44 +0200

    revisiting useState

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setcount] = useState(0);
+
+  const handleINC = () => setcount(count + 1);
+  const handleDEC = () => setcount(count - 1);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button
+        type="button"
+        className="btn"
+        onClick={handleINC}
+      >
+        Increase
+      </button>
+      <button
+        type="button"
+        className="btn"
+        onClick={handleDEC}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
+const Person = ({ name, id, remove }) => {
+  const handleRemove = () => remove(id);
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <button
+        type="button"
+        className="btn"
+        onClick={handleRemove}
+      >
+        remove
+      </button>
+    </div>
+  );
+};
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClear = () => setPeople([]);
+
+  const handleRemove = (id) =>
+    setPeople(people.filter((p) => p.id !== id));
+
+  return (
+    <div>
+      {people.map((person) => (
+        <Person
+          {...{
+            name: person.name,
+            id: person.id,
+            remove: handleRemove,
+          }}
+          key={person.id}
+        />
+      ))}
+
+      <button
+        type="button"
+        className="btn"
+        onClick={handleClear}
+        style={{ marginTop: "2rem" }}
+      >
+        Clear list
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
