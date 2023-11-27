bb36976e66e90fcc48c962f0207269a648e3eb80
Medo <mohammed.yuossry@gmail.com>
2023-11-27 10:54:19 +0200

    revisiting code splitting & useTranstion
    lazy
    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Starter from "./tutorial//11-performance/starter/03-hooks";
+import Starter from "./tutorial//11-performance/starter/04-react-18";
 import Final from "./tutorial/01-useState/final/03-useState-array";
 
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
